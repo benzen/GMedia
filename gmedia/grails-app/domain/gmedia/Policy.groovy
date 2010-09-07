2e1db52f591572aaa4d7aad117d414cb0c4f4292
Benjamin Dreux <benjiiiiii@gmail.com>
2010-09-07 13:08:40 -0400

    littel update

--- gmedia/grails-app/domain/gmedia/Policy.groovy
+++ gmedia/grails-app/domain/gmedia/Policy.groovy
@@ -2,6 +2,12 @@ package gmedia
 
 class Policy extends Resource{
 
-    static constraints = {
-    }
+  Resource resource
+  User user
+  String right
+
+  static mapping = {
+    user cascade:'all-delete-orphan'
+    resource cascade:'all-delete-orphan'
+  }
 }
--- gmedia/grails-app/domain/gmedia/User.groovy
+++ gmedia/grails-app/domain/gmedia/User.groovy
@@ -5,15 +5,13 @@ class User extends Resource{
     String name
     String password
     String email
-    Date creationDate
-    Date lastModificationDate
 
     def static constraints = {
     	email(email:true)
 	password(blank:false, password:true)
     }
-    def toString{
-    "User("+name","+email")"
+    def String toString(){
+    "User("+name+","+email+")"
     }
 
 }
